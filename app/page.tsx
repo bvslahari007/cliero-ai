09b721dff4c3b38bc368c714c50f0525c0fea016
Baswa Vinaya Sangeeta Lahari <bvslahari@gmail.com>
2026-05-28 15:25:24 +0530

    Built Cliero Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/hero";
 export default function Home() {
     return(
-        <Hero />
+        <Hero title="Cliero AI"/>
+
     );
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,12 @@
-export default function Hero(){
-    return(<div>
-        <h1>Cliero AI</h1>
+export default function Hero({title}: any) {
+    return(
+    <div className="h-screen gap-2 flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold"> {title} </h1>
+        <p className="text-lg text-gray-600">Your AI Study Companion</p>
+        <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+            GET STARTED!
+        </button>
+
+
     </div>)
 }
